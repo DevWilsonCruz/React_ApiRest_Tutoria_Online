dce8dcc9402b4e17c8e8acfa4a1b70bc786a0eaa
DevWilsonCruz <devwilsoncruz96@gmail.com>
2024-06-23 16:47:30 -0500

    Configuracion de la estructura del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import { React } from 'react'
 import { Routes,Route,Outlet} from 'react-router-dom'
 import './App.css'
 import NotFoundPage from './routes/NotFoundPage'
-
+import Home from "./routes/Home"
 function App() {
 
   return (
     <main>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/dashboard/*" element={<Outlet/>}>
           <Route path="home" element={<h1>Dashboard home</h1>}/>
         </Route>
         <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
+      
     </main>
   )
 }
---
+++ src/routes/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import IndividualIntervalsExample from "../modules/home/components/carousels/carousel"
+const Home =()=>{ 
+  return(
+    <>
+      <IndividualIntervalsExample/>
+    </>
+  )
+}
+export default Home;
